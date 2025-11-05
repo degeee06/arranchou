4e18a3c1e32fa0be8accce4a08ca0b42a9a29c00
degeee06 <cowla200@gmail.com>
2025-11-04 22:20:58 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.arranchou.app',
+  appName: 'Arranchou',
+  webDir: 'dist'
+};
+
+export default config;
