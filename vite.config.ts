aef8143f1fe2259682443019359b993116dac06f
degeee06 <cowla200@gmail.com>
2025-11-09 01:40:18 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -10,20 +11,11 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {},
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          // FIX: Use import.meta.url to get directory path in an ES module context, as __dirname is not available.
+          '@': fileURLToPath(new URL('.', import.meta.url)),
         }
-      },
-      build: {
-        rollupOptions: {
-          external: [
-            '@capacitor/app',
-            '@capacitor/browser',
-            '@capacitor/core',
-          ],
-        },
-      },
+      }
     };
 });
